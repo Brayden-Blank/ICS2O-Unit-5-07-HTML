3c9e3a3b1f9921aed8c1a2bae7af075da3b50bb9
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-05-10 16:42:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// Copyright (c) 2022 Brayden Blank All rights reserved
+//
+// Created by: Brayden Blank
+// Created on: Apr 2022
+// This file contains the JS functions for index.html
+
+"use strict";
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-5-01-HTML/sw.js", {
+    scope: "/ICS2O-Unit-5-01-HTML/",
+  });
+}
+
+const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider;
+}
+
+/**
+ * This function compares slider with random number.
+ */
+function guessClicked() {
+  const sliderValue = slider.value;
+
+  // process
+  if (sliderValue == randomNumber) {
+    document.getElementById("guess-result").innerHTML =
+      "Answer is " + randomNumber + "!" + " Congratulations!";
+    // console.log("Congratulations!")
+  }
+  //  block of code to be executed if condition1 is true
+  if (sliderValue != randomNumber) {
+    document.getElementById("guess-result").innerHTML =
+      "Answer is " + randomNumber + "!" + " Try again!";
+    // console.log("Try again!")
+  }
+}
